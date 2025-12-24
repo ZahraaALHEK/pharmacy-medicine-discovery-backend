149e288e23ae74624abb00613ebaeb28c4b4e32a
ZahraaALHEK <zahraahek628@gmail.com>
2025-12-23 20:12:32 -0800

    edit wait-for-db

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-host="$dpg-d55homer433s73dksbs0-a"
+host="$dpg-d55homer433s73dksbs0-a.internal"
 port="5432"
 
 echo "Waiting for PostgreSQL at $host:$port..."
@@ -20,4 +20,5 @@ echo "PostgreSQL is ready! Running migrations and seeders..."
 php artisan migrate --force
 php artisan db:seed --force
 # Start Laravel built-in server
-php -S 0.0.0.0:10000 -t public
+php -S 0.0.0.0:${PORT:-10000} -t public
+
